c6c2aed8bf65b364f968ef9ff548eb7a8dc18a83
YuKun Liu <mrxzx.info@gmail.com>
2023-12-29 15:10:05 -0800

    feat: add online playground

--- packages/dsx/examples/hello.rs
+++ packages/dsx/examples/hello.rs
@@ -1,17 +1,18 @@
-use dioxus::prelude::*;
-use dsx::View;
-
-fn main() {
-    dioxus_desktop::launch(App);
-}
-
-#[allow(non_snake_case)]
-fn App(cx: Scope) -> Element {
-    cx.render(rsx! {
-        div {
-            View {
-                code: "return a {{ href: \"https://github.com/mrxiaozhuox\", \"Hello World\" }}".to_string(),
-            }
-        }
-    })
-}
+// use dioxus::prelude::*;
+// use dsx::View;
+//
+// fn main() {
+//     dioxus_desktop::launch(App);
+// }
+//
+// #[allow(non_snake_case)]
+// fn App(cx: Scope) -> Element {
+//     cx.render(rsx! {
+//         div {
+//             View {
+//                 code: "return a {{ href: \"https://github.com/mrxiaozhuox\", \"Hello World\" }}".to_string(),
+//             }
+//         }
+//     })
+// }
+fn main() {}
--- packages/dsx/src/lib.rs
+++ packages/dsx/src/lib.rs
@@ -2,19 +2,28 @@ use dioscript_runtime::types::Value;
 use dioxus::prelude::*;
 
 #[allow(non_snake_case)]
-#[inline_props]
+#[component]
 pub fn View(cx: Scope, code: String) -> Element {
     let mut rt = dioscript_runtime::Runtime::new();
-    let result = rt.execute(&code).unwrap();
-    let html = match result {
-        Value::String(s) => s,
-        Value::Element(e) => e.to_html(),
-        _ => String::new(),
-    };
-    cx.render(rsx! {
-        div {
-            id: "dioscript",
-            dangerous_inner_html: "{html}"
+    let result = rt.execute(&code);
+    match result {
+        Ok(result) => {
+            let html = match result {
+                Value::String(s) => s,
+                Value::Element(e) => e.to_html(),
+                _ => String::new(),
+            };
+            cx.render(rsx! {
+                div {
+                    id: "dioscript",
+                    dangerous_inner_html: "{html}"
+                }
+            })
         }
-    })
+        Err(e) => {
+            cx.render(rsx! {
+                div { "Error: {e:?}" }
+            })
+        },
+    }
 }
